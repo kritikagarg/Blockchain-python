9ff3f4aebe260b7c06e54a7ec6f77660ec55c716
kritika_garg <kgarg.kritika@gmail.com>
2021-02-16 13:02:13 -0500

    update search_transactions method

--- README.md
+++ README.md
@@ -10,22 +10,53 @@ python blockchain.py > output.txt
 
 
 ##Working
-The blockchain is implemented in python3. 
-for a given number, "N" random transactions are made.
+A simple implementation of the blockchain for logging transactions with a single miner. The blockchain is implemented in python3.
+
+###Generate random transactions
+"N" random transactions are generated, for a given number of transactions(N), the number of customers(C) and noumber of merchants(M). The default value of N is 25, C is 5 and M is 2.  
+
 Each transaction has the following fields:
-1. Merchant_id
-2. customer_id
-3. Customer’s public key
-4. Merchant’s public key
-5. Transaction date (mmddyyyy)
-6. Transaction amount (xxxxxx.xx)
-7. Customer’s signature over the concatenation of fields 1-4
-8. Merchant’s digital signature over the concatenation of fields 1-5.
+1. Customer’s public key
+2. Merchant’s public key
+3. Transaction date (mmddyyyy)
+4. Transaction amount (xxxxxx.xx)
+5. Customer’s signature over the concatenation of fields 1-4
+6. Merchant’s digital signature over the concatenation of fields 1-5.
 
 
-Once the new Blockchain is initiated, genesis block is created. Each transaction is added to a block and then updated with following fields.   
-1. Block sequence number (for genesis block, block sequence number = 0) 
-2. Hash of the previous block’s fields 1-7.
-3. Miner’s digital signature over the concatenation of fields 6-8.
+###Initiate Blockchain
+Once the new Blockchain is initiated, genesis block is created. Each transaction is added to a new block and then following fields added by the miner:
+7. Block sequence number (for genesis block, block sequence number = 0) 
+8. Hash of the previous block’s fields 1-7.
+9. Miner’s digital signature over the concatenation of fields 6-8.
 
-The prev hash field allows to create the chain of blocks and also helps in verifying the validity of the blockchain.
+
+###Validite the blockchain & detect tampered block
+The prev hash field allows to create the chain of blocks and also helps in verifying the validity of the blockchain. To verify the blockchain, the hash of the block based on fields 1-7 is calculated again. This calculated hash is compared with the prev_hash value in the next block to validate the blockchain.
+
+Demonstrating detection of tampered block in the blockchain: 
+
+Incrementing the amount in block 10 by $10.00
+Initial amount: 72.43
+New amount: 82.43
+Verifying validity of blockchain....
+Invalid Blockchain: Block #10 is altered!!!!
+-----------------------------------
+
+###Search for transactions for a given participant(Merchant/Customer)
+
+For a given participant, ex. C3,M2, all the transactions are collected by searching using participant's public key.
+
+Demonstrating how to search for transactions using public key: 
+
+Printing all transactions for C3
+2: WC2IzJ/I4vPPl43hV SK1wOwenO1b8QwRgU 08/04/2020 $38.26
+8: WC2IzJ/I4vPPl43hV SK1wOwenO1b8QwRgU 02/13/2019 $96.36
+9: sSx6obgsd661JEQDw SK1wOwenO1b8QwRgU 07/22/2015 $50.85
+12: WC2IzJ/I4vPPl43hV SK1wOwenO1b8QwRgU 08/10/2018 $63.39
+-----------------------------------   
+
+
+
+
+
--- blockchain.py
+++ blockchain.py
@@ -3,7 +3,7 @@
 title           : blockchain.py
 author          : Kritika Garg
 date_created    : 2021-02-09
-date_modified   : 2021-02-13
+date_modified   : 2021-02-16
 usage           : python blockchain.py
                   python blockchain.py > output.txt 
 python_version  : 3.8
@@ -27,7 +27,7 @@ class Blockchain():
 		print("Creating empty blockchain...")
 		self.blockchain = []
 		#add genesis block
-		self.genesis_block = self.new_block(0,miner_key,{},None)
+		self.genesis_block, self.genesis_block_hash = self.new_block(0,miner_key,{},None)
 		self.blockchain.append(self.genesis_block)
 
 	def new_block(self, seqn, miner_key, trans, prev_hash):
@@ -37,20 +37,20 @@ class Blockchain():
 			trans.update({"seqn" : self.seqn, "mSign" : '0'})
 		else:
 			trans["seqn"] = self.seqn  #field_7
-			data = 	''.join([str(v) for k,v in trans.items() if k not in ['M','C']])
-		trans["block_hash"] = SHA256.new(data.encode('utf-8')).hexdigest()
+			data = 	''.join([str(v) for k,v in trans.items()])
+		block_hash = SHA256.new(data.encode('utf-8')).hexdigest()
 		trans["prev_hash"] = prev_hash  #field_8
 		miner_data = f'{trans["mSign"]}{trans["seqn"]}{trans["prev_hash"]}' #6-8
 		trans["minerSign"] = Transactions.Sign(self, miner_data.encode('utf-8'), miner_key[1])
-		return(trans)
+		return(trans, block_hash)
 		
 	def add_blocks(self): 
-		prev_hash = self.genesis_block["block_hash"]			
-		for i in range(1,len(transactions)+1):
-			trans = transactions[i-1]
-			block = self.new_block(i,miner_key,trans, prev_hash)
+		prev_hash = self.genesis_block_hash			
+		for seq_i in range(1,len(transactions)+1):
+			trans = transactions[seq_i-1]
+			block, block_hash = self.new_block(seq_i,miner_key,trans, prev_hash)
 			self.blockchain.append(block)	
-			prev_hash = block["block_hash"] #connecting blocks with prev_hash
+			prev_hash = block_hash #connecting blocks with prev_hash
 		self.verify_chain(self.blockchain)
 		return(self.blockchain)	
 
@@ -59,7 +59,7 @@ class Blockchain():
 		Valid_block = True
 		for i in range(1,len(blockchain)-1):
 			chain = blockchain[i]
-			data = 	''.join([str(v) for k,v in chain.items() if k not in ['M','C','block_hash','prev_hash','minerSign']])
+			data = 	''.join([str(v) for k,v in chain.items() if k not in ['prev_hash','minerSign']])
 			block_hash = SHA256.new(data.encode('utf-8')).hexdigest()
 			prev_hash = blockchain[i+1]["prev_hash"]
 			if block_hash != prev_hash:
@@ -69,13 +69,23 @@ class Blockchain():
 		if Valid_block == True:		
 			print("Blockchain is valid!")
 		print("\n")	
+		print("-----------------------------------")	
+
 
 
 class Transactions():
-	def __init__(self, n, cKeys, mKeys):
-		self.n = n
-		self.cKeys = cKeys
-		self.mKeys = mKeys
+	def __init__(self, n, No_of_customers, No_of_merchants):
+		self.n = n  #No_of_transactions
+		print(f"Generate {No_of_customers} key pairs for customers..")
+		self.cKeys = [self.generate_key_pairs() for i in range(0,No_of_customers)]
+		print(f"Generate {No_of_merchants} key pairs for merchants..")
+		self.mKeys = [self.generate_key_pairs() for i in range(0,No_of_merchants)]
+
+	def generate_key_pairs(self):
+		private_key = RSA.generate(2048)
+		public_key   = private_key.publickey().exportKey("PEM") 
+		return(public_key, private_key)	
+
 
 	def Sign(self, data, private_key):
 		# hash the data
@@ -91,6 +101,7 @@ class Transactions():
 		return(rdate.strftime("%m/%d/%Y"))
 
 	def gen_transactions(self):
+		print(f"Generate {self.n} random transactions..")
 		transactions = []
 		for i in range(0,self.n):
 			M = random.randint(1,2)
@@ -98,79 +109,74 @@ class Transactions():
 			cPrivKey = self.cKeys[C - 1][1]
 			mPrivKey = self.mKeys[M - 1][1]
 			trans = {	
-				"M" : M,
-				"C" : C,
 				"cPubKey" : self.cKeys[C - 1][0].decode("utf-8"),  #field_1
 				"mPubKey" : self.mKeys[M - 1][0].decode("utf-8"),  #field_2
 				"date" : self.random_date(),          #field_3
 				"amount" : round(random.uniform(0, 100),2) #field_4
 				}
-			data = f'{trans["cPubKey"]}{trans["mPubKey"]}{trans["date"]}{trans["amount"]}'
+			#data = f'{trans["cPubKey"]}{trans["mPubKey"]}{trans["date"]}{trans["amount"]}'
+			data = 	''.join([str(v) for k,v in trans.items()])
 			data = data.encode('utf-8')		
 			trans["cSign"] = self.Sign(data, cPrivKey)   #field_5
-			#print(type(trans["cSign"].decode()))
 			trans["mSign"] = self.Sign(data+trans["cSign"], mPrivKey) #field_6
 			transactions.append(trans)
+			print(f'{i}: M{M}({trans["mPubKey"][100:117]}) C{C}({trans["cPubKey"][100:117]}) {trans["date"]} ${trans["amount"]}')
+		print("\n")
 		return(transactions)
 
-	def print_transactions(self, transactions):
-		c=0
-		for i in transactions:
-			c+=1
-			print(f'{c}: M{i["M"]}({i["mPubKey"][100:117]}) C{i["C"]}({i["cPubKey"][100:117]}) {i["date"]} ${i["amount"]}')
-		print("Completed!")	
+	def search_transactions(self, participant, blockchain):
+		#search for transactions by public key
+		Name,idx = participant.lower()
+		nPubKey = Name+"PubKey" #keyname
+		if Name+"Keys" == 'cKeys':
+			nKeys = self.cKeys
+		elif Name+"Keys" == 'mKeys':
+			nKeys = self.mKeys
+		PubKey = nKeys[int(idx) - 1][0].decode("utf-8")  
+		participant_transactions =[]
+		print(f'Printing all transactions for {participant}')
+		for trans in blockchain[1:]:
+			if trans[nPubKey] == PubKey:
+				participant_transactions.append(trans)
+				print(f'{trans["seqn"]}: {trans["mPubKey"][100:117]} {trans["cPubKey"][100:117]} {trans["date"]} ${trans["amount"]}')
+		print("-----------------------------------")
+		return(participant_transactions)
 	
 
-def generate_key_pairs():
-	private_key = RSA.generate(2048)
-	public_key   = private_key.publickey().exportKey("PEM") 
-	return(public_key, private_key)	
-
-def search_transactions(participant, blockchain):
-	Name,idx = participant
-	participant_transactions =[]
-	for trans in blockchain[1:]:
-		if trans[Name] == int(idx):
-			participant_transactions.append(trans)
-	return(participant_transactions)
 
 
 
 if __name__ == '__main__':
-	print("Generate 5 key pairs for customers..")
-	cKeys = [generate_key_pairs() for i in range(0,5)]
-	print("Generate 2 key pairs for merchants..")
-	mKeys = [generate_key_pairs() for i in range(0,2)]
-	print("Generate 1 key pair for miner..")
-	miner_key = generate_key_pairs()
-	print("Generate 25 random transactions..")
-	No_of_transactions = 25
-	t1 = Transactions(No_of_transactions, cKeys, mKeys)
+	No_of_customers = int('5')
+	No_of_merchants = int('2')
+	No_of_transactions = int('25')	
+	t1 = Transactions(No_of_transactions, No_of_customers, No_of_merchants)
 	transactions = t1.gen_transactions()
-	t1.print_transactions(transactions)
-	print("-----------------------------------")
+
+	print("Generate 1 key pair for miner..")
+	miner_key = t1.generate_key_pairs()
 
 	full_chain = Blockchain(miner_key, transactions)
 	print("Adding Transactions to the Blockchain...")
 	blockchain = full_chain.add_blocks()
 
-	print("-----------------------------------")
-
-	print("Increment the amount in block 10 by $10.00")
+	print("Demonstrating detection of tampered block in the blockchain: \n")
+	print("Incrementing the amount in block 10 by $10.00")
 	block_trans = blockchain[10]
 	print(f'Initial amount: {block_trans["amount"]}')
 	block_trans["amount"] += 10.00
 	print(f'New amount: {block_trans["amount"]}')
 	full_chain.verify_chain(blockchain)
-	#print(blockchain)
-	#validity
-
+	
 	print("-----------------------------------")
+	print("Demonstrating how to search for transactions using public key: \n")
 	participants = ["C3","M2"]
 	for p in participants:
-		print(f'Printing all transactions for {p}')
-		p_transactions = search_transactions(p, blockchain)
-		t1.print_transactions(p_transactions)
-		print("-----------------------------------")
+		p_transactions = t1.search_transactions(p, blockchain)
+
+
+
+
+
 
 
